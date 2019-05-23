b5684698b3b2735d961de6881ff22f5f0b52a0f8
mariopedregon <mariopedregon93@gmail.com>
2019-05-23 10:31:01 -0600

    third commit

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import os
 
 
 app = Flask(__name__)
+heroku = Heroku(app)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.sqlite")
